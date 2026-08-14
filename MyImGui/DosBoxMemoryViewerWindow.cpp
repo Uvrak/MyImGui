bcbe7c01fb91d9f06994ea0a12a9923ecf47818d
Daniel Horn <danielhorn101@gmail.com>
2026-08-14 19:45:24 +0200

    Add DOSBox hex memory viewer

--- MyImGui/DosBoxMemoryViewerWindow.cpp
+++ MyImGui/DosBoxMemoryViewerWindow.cpp
@@ -1,7 +1,7 @@
 #include "pch.h"
 #include "DosBoxMemoryViewerWindow.h"
 
-#include <algorithm>
+#include <cstdio>
 
 #include "imgui.h"
 
@@ -68,54 +68,138 @@ namespace MyImGui
                 BytesPerRow
                 );
 
-        ImGuiListClipper clipper;
-
-        clipper.Begin(
-            rowCount
-        );
-
-        while (clipper.Step())
+        if (ImGui::BeginTable(
+            "##MemoryViewer",
+            18,
+            ImGuiTableFlags_Borders |
+            ImGuiTableFlags_RowBg |
+            ImGuiTableFlags_ScrollY,
+            ImVec2(
+                0.0f,
+                0.0f
+            )
+        ))
         {
-            for (int row =
-                clipper.DisplayStart;
-                row <
-                clipper.DisplayEnd;
-                ++row)
-            {
-                const size_t address =
-                    static_cast<size_t>(row) *
-                    BytesPerRow;
+            ImGui::TableSetupColumn(
+                "Address"
+            );
 
-                ImGui::Text(
-                    "%05zX",
-                    address
+            for (int column = 0;
+                column < 16;
+                ++column)
+            {
+                char label[3];
+
+                std::snprintf(
+                    label,
+                    sizeof(label),
+                    "%02X",
+                    column
                 );
 
-                ImGui::SameLine();
+                ImGui::TableSetupColumn(
+                    label
+                );
+            }
 
-                for (size_t column = 0;
-                    column < BytesPerRow;
-                    ++column)
+            
+
+            ImGui::TableSetupColumn(
+                "ASCII",
+                ImGuiTableColumnFlags_WidthFixed,
+                150.0f
+            );
+            
+
+            ImGui::TableHeadersRow();
+
+            ImGuiListClipper clipper;
+
+            clipper.Begin(
+                rowCount
+            );
+
+            while (clipper.Step())
+            {
+                for (int row =
+                    clipper.DisplayStart;
+                    row < clipper.DisplayEnd;
+                    ++row)
                 {
-                    const size_t byteAddress =
-                        address + column;
+                    const size_t address =
+                        static_cast<size_t>(row) *
+                        BytesPerRow;
 
-                    if (byteAddress >=
-                        memory.size())
-                    {
-                        break;
-                    }
+                    ImGui::TableNextRow();
 
-                    ImGui::SameLine();
+                    ImGui::TableSetColumnIndex(0);
 
                     ImGui::Text(
-                        "%02X",
-                        static_cast<unsigned int>(
-                            memory[byteAddress]
-                            )
+                        "%05zX",
+                        address
+                    );
+
+                    for (size_t column = 0;
+                        column < BytesPerRow;
+                        ++column)
+                    {
+                        const size_t byteAddress =
+                            address + column;
+
+                        ImGui::TableSetColumnIndex(
+                            static_cast<int>(
+                                column + 1
+                                )
+                        );
+
+                        if (byteAddress <
+                            memory.size())
+                        {
+                            ImGui::Text(
+                                "%02X",
+                                static_cast<unsigned int>(
+                                    memory[byteAddress]
+                                    )
+                            );
+                        }
+                    }
+                    
+                    ImGui::TableSetColumnIndex(
+                        17
+                    );
+
+                    char ascii[17] = {};
+
+                    for (size_t column = 0;
+                        column < BytesPerRow;
+                        ++column)
+                    {
+                        const size_t byteAddress =
+                            address + column;
+
+                        if (byteAddress >=
+                            memory.size())
+                        {
+                            break;
+                        }
+
+                        const uint8_t value =
+                            memory[byteAddress];
+
+                        ascii[column] =
+                            (value >= 32 &&
+                                value <= 126)
+                            ? static_cast<char>(value)
+                            : '.';
+                    }
+
+                    ImGui::TextUnformatted(
+                        ascii
                     );
                 }
             }
+
+            ImGui::EndTable();
         }
 
         ImGui::End();
